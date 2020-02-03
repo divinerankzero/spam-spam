553caa1cbd36091839544140fe0fdcc14718e836
Keith Potempa <keithrpotempa@gmail.com>
2020-02-03 14:20:31 -0600

    completed the exercise, but feel not so great about the result

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ import customers from './customers.js';
 // extract just the customers' contact email addresses 
 // and store them in a new array. 
 
+let emailList = [];
+
+let emailArys = customers.map(customer => customer.contacts.email)
+emailArys.forEach(ary => {
+    ary.forEach(email => {
+        emailList.push(email);
+    })
+})
+
+console.log("Email List Final: ", emailList)
+
+
 // You will need a nested array method - 
 // meaning one iteration inside another one - 
 // since you need to iterate the entire array of customers, 
